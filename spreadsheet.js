6da6f5df48c01b060be0b9329d2189f2f1c74375
madburger <madyarzh@gmail.com>
2020-01-14 13:29:52 +0500

    first

---
+++ spreadsheet.js
@@ -0,0 +1,76 @@
+const express = require("express");
+//const bodyParser = require('body-parser');
+const app = express();
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+  
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+/*app.use(function (req, res, next) {
+	console.log(req.body);
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.post('/', function(req, res) {
+  console.log(req.body);
+  res.jsonp({lolkek:'yougay'});
+});
+*/
+app.get('/', function(req, res) {
+
+	console.log(req.query);
+  
+	//GOOGLESHEETS
+	const GoogleSpreadsheet = require('google-spreadsheet');
+	const {promisify} = require('util');
+
+	function PrepareRows(rows){//packs rows to dataProvider to send to site to the chart
+		arr=[];
+		rows.forEach((row)=>{
+			arr.push({
+				simplecount:row.simplecount,
+				consciouscount:row.consciouscount,
+				unconsciouscount:row.unconsciouscount,
+				sended:row.sended
+			});
+		});
+		return arr;
+	}
+
+	const creds = require('./secret.json');
+	async function acessSpreadSheet(){
+		const doc = new GoogleSpreadsheet('1yBfEmVrj3Zm1rAIk1Wb-RwEEG827V54rimpG11eT3gQ');
+		await promisify(doc.useServiceAccountAuth)(creds);
+		const info = await promisify(doc.getInfo)();
+		const sheet = info.worksheets[0];
+
+		//console.log(`Title: ${sheet.title} , Rows: ${sheet.rowCount}`);
+		const rows = await promisify(sheet.getRows)({
+			offset: 1,
+			limit:200,
+			orderby:'sended'
+		});
+		res.jsonp(PrepareRows(rows));
+		//console.log(rows);
+	}
+
+	acessSpreadSheet();
+	//ENDGOOGLESHEETS
+	
+}); 
+	
+
+app.listen(3000);
